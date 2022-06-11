5107e891e9ec32fce822a4cd4ba0bb904722d01e
Moussa <mossa.tnm31@gmail.com>
2022-06-11 23:36:40 +0200

    third-commit

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const TOKEN = "OTg1Mjc0MTQ5MTIxNDMzNjkw.Gp_bR_.Ear2f3H_2DAQ4CMf4heZzF6Rr-tCTUouHVSXws"
+const TOKEN = "OTg1Mjc0MTQ5MTIxNDMzNjkw.GC_2Y6.0jH3liHeYNEsVKRw_Xrw3_8rX_g-A2FjGRzgbQ"
 const { Client, Intents } = require('discord.js');
 const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES] });
 
@@ -9,6 +9,10 @@ client.on('ready', ()=>{
 client.on('message', msg =>{
     if(msg.content.toLowerCase() === 'hello'){
         msg.reply(`${msg.author} World!`)
+    }
+
+    if(msg.channel.id === '985288693189328997'){
+        client.channels.cache.get('985273790592319552').send({ content: `${client.user.tag} shared ressource, check it out` });
     }
 })
 
